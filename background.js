41dd79c9db88755172737fbebf0894bdac8b82bb
matt <63452179+mathieufitzgerald@users.noreply.github.com>
2024-01-24 08:41:23 +0100

    major update

--- background.js
+++ background.js
@@ -1,38 +1,48 @@
-function switchTabs() {
-    chrome.windows.create({state: "fullscreen"}, function(win) {
-        chrome.storage.sync.get(['urls'], function(result) {
-            const urls = result.urls || ['https://www.managebac.com'];
-            urls.forEach((url, index) => {
-                if(index == 0) {
-                    // Update the initial tab created with the window
-                    chrome.tabs.update(win.tabs[0].id, {url: url});
-                } else {
-                    // Create new tabs in the same window
-                    chrome.tabs.create({windowId: win.id, url: url});
-                }
-            });
-  
-            // Close all other windows
-            chrome.windows.getAll(function(wins) {
-                for(let i = 0; i < wins.length; i++) {
-                    if(wins[i].id != win.id) {
-                        chrome.windows.remove(wins[i].id);
-                    }
-                }
-            });
+function handlePreviousWindows(currentWinId) {
+    chrome.storage.sync.get(['closeOrMinimize'], function(result) {
+      const shouldMinimize = result.closeOrMinimize || false;
+      chrome.windows.getAll({ populate: true }, function(windows) {
+        windows.forEach(function(window) {
+          if (window.id !== currentWinId) {
+            if (shouldMinimize) {
+              chrome.windows.update(window.id, { state: 'minimized' });
+            } else {
+              chrome.windows.remove(window.id);
+            }
+          }
         });
+      });
     });
 }
 
+// Updated switchTabs function
+function switchTabs() {
+    chrome.windows.create({state: "fullscreen"}, function(win) {
+      chrome.storage.sync.get(['urls'], function(result) {
+        const urls = result.urls || ['https://www.managebac.com'];
+        urls.forEach((url, index) => {
+          if(index == 0) {
+            // Update the initial tab created with the window
+            chrome.tabs.update(win.tabs[0].id, {url: url});
+          } else {
+            // Create new tabs in the same window
+            chrome.tabs.create({windowId: win.id, url: url});
+          }
+        });
+  
+        // Handle previous windows based on user preference
+        handlePreviousWindows(win.id);
+      });
+    });
+}
+
+// Listener for the extension icon click
 chrome.action.onClicked.addListener((tab) => {
     switchTabs();
 });
-  
+
 chrome.commands.onCommand.addListener(function(command) {
     if (command === "switch_tabs") {
       switchTabs();
     }
 });
-
-
-
